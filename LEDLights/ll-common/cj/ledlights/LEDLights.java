a3c5b62dc7656643e589a3cb971d72ce876e7ecd
SiDzej <Inaros2010@gmail.com>
2013-05-25 23:02:39 +0200

    Huh, maybe start commit finaly

---
+++ LEDLights/ll-common/cj/ledlights/LEDLights.java
@@ -0,0 +1,29 @@
+package cj.ledlights;
+
+import cj.ledlights.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class LEDLights {
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@Init
+	public void Init(FMLInitializationEvent event){
+		
+	}
+	
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+}
