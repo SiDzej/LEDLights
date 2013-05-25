4c5553c03b12b86d645dba04c4c3ef8c683c7a25
SiDzej <Inaros2010@gmail.com>
2013-05-26 00:03:16 +0200

    hmm

--- LEDLights/ll-common/cj/ledlights/lib/Reference.java
+++
@@ -1,8 +0,0 @@
-package cj.ledlights.lib;
-
-public class Reference {
-	
-	public static final String MOD_ID = "LEDLights";
-	public static final String MOD_NAME = "LED Lights";
-	public static final String VERSION = "0.0.1";
-}
